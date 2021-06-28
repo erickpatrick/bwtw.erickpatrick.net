ef420805011fd4e99478be085d40c5dfa82caf26
erick patrick <github@erickpatrick.net>
2021-06-28 23:28:59 +0200

    configures tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   jit: true,
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      './layouts/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      keyframes: true,
+      fontFace: true,
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+    },
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   variants: {
