83e307b313de714fe8c587f5dae354f915ffec49
erick patrick <github@erickpatrick.net>
2021-07-28 14:51:24 +0200

    generates script to create listing blog json

---
+++ scripts/generate-blogs.mjs
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import globby from 'globby';
+import { createCompiler } from '@mdx-js/mdx';
+import visit from 'unist-util-visit';
+
+(async () => {
+  const files = await globby([
+    'pages/blog/**/*.(mdx)',
+    '!pages/blog/_*.(js|ts|jsx|tsx|mdx)',
+    '!pages/api',
+  ]);
+
+  const posts = files.map((file) => {
+    const link = file.replace('pages/', '').replace('.mdx', '');
+    const finalFile = fs.readFileSync(process.cwd() + '/' + file);
+
+    const extractMeta = () => (tree, file) =>
+      visit(tree, 'export', function visitor(node) {
+        if (node.value.includes('const meta')) {
+          const value = node.value;
+          const finalMeta = value
+            .replace('export const meta = ', '(() => (')
+            .replace('};', '}))()');
+
+          file.data.frontmatter = eval(finalMeta);
+        }
+      });
+    const compiler = createCompiler({
+      remarkPlugins: [extractMeta],
+    });
+
+    let meta = {};
+
+    compiler.process(finalFile, function done(err, file) {
+      meta = file.data.frontmatter;
+    });
+
+    return JSON.stringify({
+      link,
+      ...meta,
+    });
+  });
+
+  // eslint-disable-next-line no-sync
+  fs.writeFile('pages/blog/posts.json', `{"posts":[${posts}]}`, () => ({}));
+})();
