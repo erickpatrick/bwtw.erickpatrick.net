b9f5c8620ae2c581e5cdba0f652768b5a5ce20ec
erick patrick <github@erickpatrick.net>
2021-07-29 17:34:38 +0200

    adds new internal page header with proper menu

--- layouts/partials/BlogHeader.tsx
+++ layouts/partials/BlogHeader.tsx
@@ -1,7 +1,49 @@
+import Link from 'next/link';
+
 export default function BlogHeader() {
   return (
-    <header className="p-4 flex bg-black justify-center items-center text-center text-white">
-      blog header
+    <header className="flex bg-black justify-between items-center text-center text-white">
+      <div>
+        <p>
+          <Link href="/">
+            <a className="inline-block p-4 text-2xl font-bold hover:bg-white hover:text-black">
+              Erick Patrick
+            </a>
+          </Link>
+        </p>
+      </div>
+      <div className="hidden sm:block">
+        <nav>
+          <Link href="/projects">
+            <a className="inline-block p-5 text-base font-bold hover:bg-white hover:text-black">
+              Projects
+            </a>
+          </Link>
+          <Link href="/blog">
+            <a className="inline-block p-5 text-base font-bold hover:bg-white hover:text-black">
+              Blog
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a className="inline-block p-5 text-base font-bold hover:bg-white hover:text-black">
+              Contact
+            </a>
+          </Link>
+          <Link href="/privacy">
+            <a className="inline-block p-5 text-base font-bold hover:bg-white hover:text-black">
+              Privacy
+            </a>
+          </Link>
+        </nav>
+      </div>
+      <div className="sm:hidden">
+        <a
+          href="#menu"
+          className="inline-block p-5 text-base font-bold hover:bg-white hover:text-black"
+        >
+          Menu
+        </a>
+      </div>
     </header>
   );
 }
