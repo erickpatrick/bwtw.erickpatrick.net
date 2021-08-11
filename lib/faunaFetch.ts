b2c24e72f21630cd1564df148830eb3d1256529d
erick patrick <github@erickpatrick.net>
2021-08-11 13:30:18 +0200

    adds fauna related scripts

---
+++ lib/faunaFetch.ts
@@ -0,0 +1,31 @@
+export type FaunaFetcher = {
+  name: string;
+  email: string;
+  website: string;
+  message: string;
+};
+export default function fetcher(
+  action: string,
+  { name, email, website, message }: FaunaFetcher
+) {
+  fetch(action, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: name,
+      website: website,
+      email: email,
+      message: message,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        res.json();
+      } else {
+        Promise.reject(res).catch(() => false);
+      }
+    })
+    .catch((data) => console.log(data));
+}
